f49bf0788767a7a80984a08054813d08ef6f2eb1
salkatib <47203494+salkatib@users.noreply.github.com>
2020-11-24 03:17:45 -0500

    Delete under_construction.php

--- under_construction.php
+++
@@ -1,6 +0,0 @@
-<?php include 'view/header.php'; ?>
-<main>
-    <h2>Sorry, this page is currently under construction.</h2>
-    <p>We'll finish it as quickly as we can. Thanks!</p>
-</main>
-<?php include 'view/footer.php'; ?>
